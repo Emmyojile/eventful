7099df7dcdc47dbcf4ee2000e46ec624ea033a7b
Emmyojile <emmaojile99@gmail.com>
2024-02-08 22:26:26 +0100

    adding shadcn ui lib and header ui

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">Header</div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
