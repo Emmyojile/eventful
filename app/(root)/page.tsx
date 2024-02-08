c637a22a39cf24533c7e8c99b8340ce7b87ded64
Emmyojile <emmaojile99@gmail.com>
2024-02-08 12:41:18 +0100

    adding shadcn ui lib and header ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
+      <h1 className=""></h1>
         <Button variant="destructive" className="px-10">Hello</Button>
     </main>
   );
