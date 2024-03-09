61165346a4bf51029da284dda58b14123dd9bbe6
Emmyojile <emmaojile99@gmail.com>
2024-03-09 22:16:03 +0100

    fixed server actions issues and getting single events based on collection

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import { IEvent } from "@/lib/database/models/event.model";
+import { formatDateTime } from "@/lib/utils";
+import Link from "next/link";
+import React from "react";
 
-const Card = () => {
+type CardProps = {
+  event: IEvent;
+  hasOrderLink?: boolean;
+  hidePrIce?: boolean;
+};
+
+const Card = ({ event, hasOrderLink, hidePrIce }: CardProps) => {
   return (
-    <div>Card</div>
-  )
-}
+    <div className="group relative flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl bg-white shadow-md transition-all hover:shadow-lg md:min-h-[438px]">
+      <Link
+        href={`/events/${event._id}`}
+        style={{ backgroundImage: `url(${event.imageUrl})` }}
+        className="flex-center flex-grow bg-gray-50 bg-cover bg-center text-grey-500"
+      />
+      <Link
+        href={`/events/${event._id}`}
+        className="flex min-h-[230px] flex-col gap-3 p-5 md:gap-4"
+      >
+        {!hidePrIce && (
+          <div className="flex gap-2">
+            <span className="p-semibold-14 w-min rounded-full bg-green-100 px-4 py-1 text-green-60">
+              {event.isFree ? "FREE" : `$${event.price}`}
+            </span>
+            <p className="p-semibold-14 w-min rounded-full bg-grey-500/10 px-4 py-1 text-grey-500 line-clamp-1">
+              {event.category.name}
+            </p>
+          </div>
+        )}
 
-export default Card
+        <p className="p-medium-16 p-medium-18 text-grey-500">
+          {formatDateTime(event.startDateTime).dateTime}
+        </p>
+        <p className="p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black">
+          {event.title}
+        </p>
+      </Link>
+    </div>
+  );
+};
+
+export default Card;
